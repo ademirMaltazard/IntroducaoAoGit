8417700c4dc7356598e89b672d4839e2e90be2bd
Ademir Gama <159483317+ademirMaltazard@users.noreply.github.com>
2024-04-17 22:58:53 +0000

    resolução da quarta atividade da segunda aula

---
+++ 2-quartaQuestão.py
@@ -0,0 +1,8 @@
+# Crie uma função em python que receba um número diga se o número é par ou ímpar.
+
+numero = int(input('digite um numero: '))
+
+if (numero%2) == 0:
+    print('O numero é par')
+else:
+    print('O numero é ímpar')
