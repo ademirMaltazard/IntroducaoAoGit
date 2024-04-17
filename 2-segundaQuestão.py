8acfcc8c0fa9531d3c22e41543231c10b556df5b
Ademir Gama <159483317+ademirMaltazard@users.noreply.github.com>
2024-04-17 22:39:47 +0000

    Resolucao da segunda atividade da segunda aula

---
+++ 2-segundaQuestão.py
@@ -0,0 +1,18 @@
+numero = int(input('digite um numero: '))
+
+for indice in range(0, 10):
+    print('Tabuada de multiplicação:')
+    print(f'{numero} x {indice} = {numero * indice}')
+
+for indice in range(0, 10):
+    print('Tabuada de subtração:')
+    print(f'{numero} - {indice} = {numero - indice}')
+
+for indice in range(0, 10):
+    print('Tabuada de soma:')
+    print(f'{numero} + {indice} = {numero + indice}')
+
+for indice in range(0, 10):
+    print('Tabuada de divisao:')
+    print(f'{numero} / {indice} = {numero / indice}')
+
